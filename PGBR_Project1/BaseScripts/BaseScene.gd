0423f0bfcb9557c0efc25e4e875ca231286ef035
manglemix <38180553+manglemix@users.noreply.github.com>
2020-05-09 01:17:11 +0800

    Create BaseScene.gd

---
+++ PGBR_Project1/BaseScripts/BaseScene.gd
@@ -0,0 +1,21 @@
+extends Spatial
+
+
+#var factions := {}
+#
+#var _AI_threads := []
+#var _mutex := Mutex.new()
+#
+#
+#func _ready():
+#	# hides the mouse
+#	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+#
+#
+#func activate_AI(faction: String, AI_type: String):
+#	_AI_threads.append(Thread.new())
+#	_AI_threads[-1].start(self, AI_type, faction)
+#
+#
+#func Loner_AI(faction: String):
+#	pass
