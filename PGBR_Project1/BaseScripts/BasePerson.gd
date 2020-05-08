52214ac126d2f38d65e1c014c08678e533c6f2f6
manglemix <38180553+manglemix@users.noreply.github.com>
2020-05-08 17:15:03 +0800

    Fixed movement mechanics

--- PGBR_Project1/BaseScripts/BasePerson.gd
+++ PGBR_Project1/BaseScripts/BasePerson.gd
@@ -4,6 +4,7 @@ extends KinematicBody
 
 signal shoot		# when emitted, all gun nodes connected to this should shoot
 signal died			# may or may not be needed, we'll be watched by the current scene
+signal aim
 
 var SPRINT := 20.0
 var RUN := 10.0
@@ -12,6 +13,7 @@ var AIR := 5.0
 
 var move_speed := 10.0					# the top down speed of the person
 var movement_vector := Vector3.ZERO		# the top down velocity of the person
+var acceleration := 6
 var jump_speed := 10.0					# the vertical speed given to the person when they jump
 var vertical_speed := 0.0				# we separate the vertical speed to make things easier
 var fall_acceleration := - 9.8			# the rate at which the vertical speed changes, it is unique to each Person as they may have parachutes
@@ -19,6 +21,7 @@ var linear_velocity := Vector3.ZERO
 var charging_jump := false				# if true, the Person will try to charge up its jump strength
 var on_floor: bool						# if true, the KinematicBody is on top of a floor. is_on_floor() is only true if the KinematicBody is in the floor
 
+var _floor_collision: KinematicCollision
 var _jump_charge_start: int				# the system time in msecs when a jump began to charge
 var _jump_charge_target: float			# the target strength of the jump
 var _jump_charge_factor := 0.001		# jump strength units per millisecond
@@ -34,11 +37,6 @@ func move_to_vector(vector: Vector3, speed:=RUN):
 		movement_vector += vector * AIR * get_physics_process_delta_time()
 
 
-func stop_moving():
-	if on_floor:
-		movement_vector = Vector3.ZERO
-
-
 func charge_jump(strength:=1.5):
 	if not charging_jump:
 		charging_jump = true
@@ -50,10 +48,11 @@ func charge_jump(strength:=1.5):
 func jump():
 	if on_floor:
 		print((OS.get_system_time_msecs() - _jump_charge_start) * _jump_charge_factor + 1)
-		vertical_speed = jump_speed
+		linear_velocity.y += jump_speed
+		on_floor = false
 		
 		if charging_jump:
-			vertical_speed += jump_speed * (OS.get_system_time_msecs() - _jump_charge_start) * _jump_charge_factor
+			linear_velocity.y += jump_speed * (OS.get_system_time_msecs() - _jump_charge_start) * _jump_charge_factor
 	
 		charging_jump = false
 
@@ -63,20 +62,24 @@ func shoot_guns():
 
 
 func _physics_process(delta):
-	on_floor = test_move(global_transform, Vector3.DOWN * 0.001)
-	# flatten the linear velocity first so the interpolation is only along the x and z axes
-	linear_velocity.y = 0.0
-	linear_velocity = linear_velocity.linear_interpolate(movement_vector, 0.1)
+	_floor_collision = move_and_collide(Vector3.DOWN * 0.001, true, true, true)
+	on_floor = is_instance_valid(_floor_collision)
 	
 	if on_floor:
+		var axis := Vector3.UP.cross(_floor_collision.normal).normalized()
+		var angle := Vector3.UP.angle_to(_floor_collision.normal)
+		movement_vector = movement_vector.rotated(axis, angle)
+		linear_velocity = linear_velocity.linear_interpolate(movement_vector, acceleration * delta)
+		
+		vertical_speed = 0.0
 		if charging_jump and (OS.get_system_time_msecs() - _jump_charge_start) * _jump_charge_factor >= _jump_charge_target - 1:
 			jump()
-		
-	else:
-		vertical_speed += fall_acceleration * delta
-		charging_jump = false
 	
-	linear_velocity.y = vertical_speed
+	else:
+		charging_jump = false
+		linear_velocity += movement_vector * delta
+		linear_velocity.y += fall_acceleration * delta
 	
 	linear_velocity = move_and_slide(linear_velocity, Vector3.UP)
-	vertical_speed = linear_velocity.y
+	movement_vector *= 0.0
+	Debug.draw_points([global_transform.origin, global_transform.origin + linear_velocity], Color.red)
