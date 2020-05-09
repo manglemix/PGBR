591c5d34bc384945e34ae457826bab4a994f4ba3
manglemix <38180553+manglemix@users.noreply.github.com>
2020-05-09 21:45:58 +0800

    removed previous draft for AI

--- PGBR_Project1/BaseScripts/BaseScene.gd
+++ PGBR_Project1/BaseScripts/BaseScene.gd
@@ -6,18 +6,3 @@ export var enable_debug := false
 
 func _ready():
 	Debug.enabled = enable_debug
-
-
-#var factions := {}
-#
-#var _AI_threads := []
-#var _mutex := Mutex.new()
-#
-#
-#func activate_AI(faction: String, AI_type: String):
-#	_AI_threads.append(Thread.new())
-#	_AI_threads[-1].start(self, AI_type, faction)
-#
-#
-#func Loner_AI(faction: String):
-#	pass
