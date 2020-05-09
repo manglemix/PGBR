0d970d08a2c2079be8f3217493c4849e7742bcfc
manglemix <38180553+manglemix@users.noreply.github.com>
2020-05-10 00:26:52 +0800

    Update ActionTree.gd

--- PGBR_Project1/UtilityAI/ActionTree.gd
+++ PGBR_Project1/UtilityAI/ActionTree.gd
@@ -2,10 +2,19 @@ class_name ActionTree
 extends Node
 
 
-onready var scene := get_tree().get_current_scene()
+export(String) var group
+export var active := true		# if true, all nodes in the group will be controlled by this node
+
+var assigned_nodes := {}
 
 
 func get_best_action(employee):
 	assert(get_child_count() == 0)
-	get_child(0).get_score()
+	get_child(0).get_score(employee)
 	return get_child(0).best_action
+
+
+func _process(delta):
+	if active:
+		var nodes := get_tree().get_nodes_in_group(group)
+		# TODO FINISH
