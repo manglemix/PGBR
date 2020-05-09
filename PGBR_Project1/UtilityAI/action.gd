8737fef5ab1a921ff95ee709d04cd54f8c6e1123
manglemix <38180553+manglemix@users.noreply.github.com>
2020-05-09 21:42:43 +0800

    Added Action and Maximiser nodes
    These are for the General Purpose Utility AI. Basically the Action node has a method called get_score which is given the node which is asking for an action, and the current scene. The Action Node then returns its associated score. If the AI tree, starts with a Maximiser node however, it will look at all its children and pick the child with the highest score

---
+++ PGBR_Project1/UtilityAI/action.gd
@@ -0,0 +1,11 @@
+class_name Action
+extends Node
+
+
+var best_action = self
+
+
+func get_score(employee, scene) -> float:
+	# employee is the node that is asking for the best action to do
+	# this function returns how much 'value' there is when using the best_action offered by this script
+	return 0.0
---
+++ PGBR_Project1/UtilityAI/maximiser.gd
@@ -0,0 +1,21 @@
+class_name Maximiser
+extends Action
+
+
+func get_score(employee, scene) -> float:
+	# this node looks at its children and finds which one has the highest score, and shows it off
+	# if that score was the highest, it will be offered as the best_action
+	var max_score: float
+	
+	for child in get_children():
+		var score = child.get_score(employee, scene)
+		
+		if is_inf(score):
+			best_action = child
+			break
+		
+		if score > max_score:
+			best_action = child
+			max_score = score
+	
+	return max_score
