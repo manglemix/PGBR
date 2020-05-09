a2a42a901aa2f239d94771059b1e5aa81a92c767
manglemix <38180553+manglemix@users.noreply.github.com>
2020-05-09 22:15:27 +0800

    Added execute method
    This method is the main way the Action node controls its employee

--- PGBR_Project1/UtilityAI/action.gd
+++ PGBR_Project1/UtilityAI/action.gd
@@ -2,6 +2,9 @@ class_name Action
 extends Node
 
 
+const CAN_CHANGE := 1			# this node can be changed, but can also remain the as the current action without resetting
+const MUST_CHANGE := 2			# this node must be reset if ran again, or it another node can take over
+
 var best_action = self
 
 
@@ -9,3 +12,9 @@ func get_score(employee, scene) -> float:
 	# employee is the node that is asking for the best action to do
 	# this function returns how much 'value' there is when using the best_action offered by this script
 	return 0.0
+
+
+func execute() -> int:
+	# runs the code which causes my_employee to complete a task
+	# must return an error code to alert the ActionTree to check for the best action again
+	return OK
