2bacd1698a121a1c191a44bb965836d930531aa0
manglemix <38180553+manglemix@users.noreply.github.com>
2020-05-09 22:21:14 +0800

    Added reset method
    This method prepares the node to control its employee

--- PGBR_Project1/UtilityAI/action.gd
+++ PGBR_Project1/UtilityAI/action.gd
@@ -6,12 +6,18 @@ const CAN_CHANGE := 1			# this node can be changed, but can also remain the as t
 const MUST_CHANGE := 2			# this node must be reset if ran again, or it another node can take over
 
 var best_action = self
+var employee					# this value is forcefully set by Maximiser or ActionTree
 
 
 func get_score(employee, scene) -> float:
 	# employee is the node that is asking for the best action to do
 	# this function returns how much 'value' there is when using the best_action offered by this script
 	return 0.0
+
+
+func reset() -> void:
+	# if there are any variables that must be reset so that the employee given can run
+	return
 
 
 func execute() -> int:
