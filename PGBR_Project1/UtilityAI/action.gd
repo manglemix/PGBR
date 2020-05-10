29eca0e68f4703a33e6250487ff0660e3774169c
manglemix <38180553+manglemix@users.noreply.github.com>
2020-05-10 15:47:30 +0800

    Reworked the Action node
    The previous design only allowed for one AI at a time to use an ActionTree. What should really happen is the ActionTree decides, for each node, what the best action is. It should then delegate the processing of that action to a separate object (a reference in this case). That way, the nodes in the ActionTree can return to deciding the best action for a new employee

--- PGBR_Project1/UtilityAI/action.gd
+++ PGBR_Project1/UtilityAI/action.gd
@@ -2,10 +2,6 @@ class_name Action
 extends Node
 
 
-const CAN_CHANGE := 1			# this node can be changed, but can also remain the as the current action without resetting
-const MUST_CHANGE := 2			# this node must be reset if ran again, or it another node can take over
-
-var best_action = self
 var employee					# this value is forcefully set by Maximiser or ActionTree
 
 
@@ -15,16 +11,18 @@ func get_score() -> float:
 	return 0.0
 
 
-func reset() -> void:
-	# if there are any variables that must be reset so that the employee given can run
-	return
+class ActionInterface:
+	var employee
+	
+	func _init(node):
+		employee = node
+	
+	func process(delta) -> int:
+		return OK
+	
+	func end():
+		return
 
 
-func execute() -> int:
-	# runs the code which causes employee to complete a task
-	# must return an error code to alert the ActionTree to check for the best action again
-	return OK
-
-
-func end() -> void:
-	return
+func get_best_action(employee) -> Reference:
+	return ActionInterface.new(employee)
