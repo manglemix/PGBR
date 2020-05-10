902e7807cc38c857c23df0f4b73c78b52552cd9a
manglemix <38180553+manglemix@users.noreply.github.com>
2020-05-10 16:05:52 +0800

    Added get_action_tree method

--- PGBR_Project1/UtilityAI/action.gd
+++ PGBR_Project1/UtilityAI/action.gd
@@ -5,7 +5,12 @@ extends Node
 var employee					# this value is forcefully set by Maximiser or ActionTree
 
 
-func get_score() -> float:
+func get_action_tree() -> ActionTree:
+	if not get_parent() is ActionTree:
+		return get_parent().get_action_tree()
+	
+	return get_parent() as ActionTree
+
 	# employee is the node that is asking for the best action to do
 	# this function returns how much 'value' there is when using the best_action offered by this script
 	return 0.0
