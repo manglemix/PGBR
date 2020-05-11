8da43cc55686e98867df4390e43f0aa511460599
manglemix <38180553+manglemix@users.noreply.github.com>
2020-05-11 12:04:08 +0800

    Refactored StaminaBar

--- PGBR_Project1/Scenes/HUD/StaminaBar.gd
+++ PGBR_Project1/Scenes/HUD/StaminaBar.gd
@@ -1,15 +1,14 @@
+class_name StaminaBar
 extends Control
 
-class_name StaminaBar
 
-onready var stamina_bar := $StaminaBar as TextureProgress
-onready var update_tween := $UpdateTween as Tween
-onready var player := get_tree().get_root().get_node("Draft/Person") as BasePerson
-
-func map(x: float, in_min: float, in_max: float, out_min: float, out_max: float):
-	return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min
+#func map(x: float, in_min: float, in_max: float, out_min: float, out_max: float):
+#	return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min
 
 
-func _process(delta):
-	stamina_bar.max_value = player.max_stamina
-	stamina_bar.value = player.curr_stamina
+func _on_max_stamina_updated(max_stamina):
+	$StaminaBar.max_value = max_stamina
+
+
+func _on_stamina_updated(stamina):
+	$StaminaBar.value = stamina
