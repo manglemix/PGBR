70acff133b386bfe640b864f6b46bbb734de82cc
manglemix <38180553+manglemix@users.noreply.github.com>
2020-05-14 18:56:13 +0800

    Finished AutoGoto
    Now in beta

--- PGBR_Project1/UtilityAI/auto_goto.gd
+++ PGBR_Project1/UtilityAI/auto_goto.gd
@@ -3,27 +3,26 @@ extends Node
 
 
 signal destination_reached
+signal no_path
 
-export var _origin: Vector3
-export var _destination: Vector3
+export var destination: Vector3
 export var completion_distance := 2.0
 export var enabled := true setget set_enabled
 export var turn_head := true
+export var safe_distance := 0.5
 
-var path: PoolVector3Array
-
+var _path: PoolVector3Array
 var _optimize := true
 onready var _navigation := get_tree().get_current_scene().get_node("Navigation") as Navigation
 
 
-func _init(from: Vector3, to: Vector3, optimize:=true):
-	_origin = from
-	_destination = to
+func _init(to: Vector3, optimize:=true):
+	destination = to
 	_optimize = optimize
 
 
 func _ready():
-	set_path(_origin, _destination, _optimize)
+	set_path(destination, _optimize)
 
 
 func set_enabled(value: bool):
@@ -31,31 +30,47 @@ func set_enabled(value: bool):
 	set_process(value)
 
 
-func set_path(from: Vector3, to: Vector3, optimize:=true):
-	_origin = _navigation.get_closest_point(from)
-	_destination = _navigation.get_closest_point(to)
+func set_path(to: Vector3, optimize:=true):
+	destination = _navigation.get_closest_point(to)
 	_optimize = optimize
 	
-	path = _navigation.get_simple_path(_origin, _destination, _optimize)
-	enabled = true
+	_path = _navigation.get_simple_path(_navigation.get_closest_point(get_parent().global_transform.origin), destination, _optimize)
+	
+	if _path.empty():
+		emit_signal("no_path")
+		set_enabled(false)
+	else:
+		set_enabled(true)
+
+
+func get_navigation_path() -> PoolVector3Array:
+	return _path
 
 
 func _process(delta):
-	if len(path) == 0:
-		emit_signal("destination_reached")
-		set_enabled(false)
-		return
+	if get_parent().global_transform.origin.distance_to(_path[0]) < completion_distance:
+		_path.remove(0)
+		
+		if _path.empty():
+			emit_signal("destination_reached")
+			set_enabled(false)
+			return
 	
-	if get_parent().global_transform.origin.distance_to(path[0]) < completion_distance:
-		path.remove(0)
-		return
+	var collision := get_parent().move_and_collide(_path[0] - get_parent().global_transform.origin, true, true, true) as KinematicCollision
 	
-	get_parent().global_move_to_vector(path[0])
+	if is_instance_valid(collision):
+		if _path.size() > 1:
+			# this pushes the next path away from the parent and the next point
+			# if the next point is at a corner, this should end up pushing it away from the corner
+			_path[0] = _navigation.get_closest_point(_path[0] + ((_path[0] - get_parent().global_transform.origin).normalized() + (_path[0] - _path[1]).normalized()) * safe_distance)
+
+	
+	get_parent().global_move_to_vector(_path[0])
 	
 	if turn_head:
-		get_parent().global_head_to_vector(path[0])
+		get_parent().global_head_to_vector(_path[0])
 	
 	if Debug.enabled:
-		var debug_path := path
+		var debug_path := _path
 		debug_path.insert(0, get_parent().global_transform.origin)
-		Debug.draw_points(debug_path)
+		Debug.draw_points(debug_path, Color.black)
