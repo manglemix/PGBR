bf3aa1ca138b84cc62b24e22914739f8739f920e
manglemix <38180553+manglemix@users.noreply.github.com>
2020-05-14 18:55:40 +0800

    Added get_score to Wander

--- PGBR_Project1/UtilityAI/wander.gd
+++ PGBR_Project1/UtilityAI/wander.gd
@@ -6,12 +6,19 @@ export var min_distance := 5.0
 export var max_distance := 10.0
 
 
+func get_score(employee) -> float:
+	for group in get_parent().hostile_groups:
+		if len(get_tree().get_nodes_in_group(group)) > 0:
+			return 0.0
+	
+	return INF
+
+
 func _process(delta):
 	var goto := get_parent().get_node_or_null("Goto") as AutoGoto
 	
 	if not is_instance_valid(goto):
-		goto = AutoGoto.new(get_parent().global_transform.origin,
-		get_parent().global_transform.origin + Vector3.BACK.rotated(Vector3.UP, rand_range(0, TAU)) * rand_range(min_distance, max_distance))
+		goto = AutoGoto.new(get_parent().global_transform.origin + Vector3.BACK.rotated(Vector3.UP, rand_range(0, TAU)) * rand_range(min_distance, max_distance))
 		
 		get_parent().add_child(goto)
 		goto.name = "Goto"
