da0c049f7abc15dcc3878d1ff882361762ee66b3
manglemix <38180553+manglemix@users.noreply.github.com>
2020-05-14 18:54:37 +0800

    Improved maximiser
    Added a signal called calling_get_score which is emitted immediately before any child state is picked in get_score. this is For virtual environments who need to initialise variables for their children to use

--- PGBR_Project1/UtilityAI/maximiser.gd
+++ PGBR_Project1/UtilityAI/maximiser.gd
@@ -2,13 +2,16 @@ class_name Maximiser
 extends State
 
 
+signal calling_get_score(employee)
+
 var _best_node: State
 
 
 func get_score(employee) -> float:
+	emit_signal("calling_get_score", employee)
 	# this node looks at its children and finds which one has the highest score, and shows it off
 	# if that score was the highest, it will be offered as the best state
-	var max_score: float
+	var max_score := -INF
 	
 	for child in get_children():
 		var score = child.get_score(employee)
@@ -24,5 +27,5 @@ func get_score(employee) -> float:
 	return max_score
 
 
-func get_state():
-	return _best_node
+func get_state(employee):
+	return _best_node.get_state(employee)
