95ee481dc9a603ebf2537bbbbc4d934f289c1506
manglemix <38180553+manglemix@users.noreply.github.com>
2020-05-14 18:57:30 +0800

    Finished Attack
    Now in beta

--- PGBR_Project1/UtilityAI/attack.gd
+++ PGBR_Project1/UtilityAI/attack.gd
@@ -12,19 +12,19 @@ func get_score(employee: BasePerson) -> float:
 	# employee is the node that is asking for the best action to do
 	# this function returns how much 'value' there is when using the best_action offered by this script
 	
-	var valid_target := []
-	for group in get_parent().hostile_groups:
-		for enemy in get_tree().get_nodes_in_group(group):
-			var collision = employee.get_world().direct_space_state.intersect_ray(employee.get_node("Head").global_transform.origin, enemy.global_transform.origin, [employee])
-			
-			if not collision.empty() and collision["collider"] == enemy:
-				valid_target.append(enemy)
-	
-	if valid_target.empty():
-		return 0.0
+#	var valid_target := []
+#	for group in get_parent().hostile_groups:
+#		for enemy in get_tree().get_nodes_in_group(group):
+#			var collision = employee.get_world().direct_space_state.intersect_ray(employee.get_node("Head").global_transform.origin, enemy.global_transform.origin, [employee])
+#
+#			if not collision.empty() and collision["collider"] == enemy:
+#				valid_target.append(enemy)
+#
+#	if valid_target.empty():
+#		return 0.0
 	
 	var minimum := INF
-	for enemy in valid_target:
+	for enemy in get_parent().hostile_nodes:
 		var distance = employee.global_transform.origin.distance_to(enemy.global_transform.origin)
 		
 		if distance < minimum:
@@ -40,7 +40,7 @@ func get_state(employee):
 	return state
 
 
-func _process(delta):
+func _process(_delta):
 	var goto = get_parent().get_node_or_null("Goto") as AutoGoto
 	
 	if not is_instance_valid(_target):
@@ -49,7 +49,7 @@ func _process(delta):
 			goto.queue_free()
 		return
 	
-	get_parent().fully_face_target(_target.global_transform.origin)
+	get_parent().fully_face_target(_target.get_node("Body").global_transform.origin)
 	
 	if get_parent().guns[0].get_collider() == _target:
 		get_parent().shoot_guns()
