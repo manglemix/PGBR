0516d54b77ab6d8c978f7476b1447eee56e36fba
manglemix <38180553+manglemix@users.noreply.github.com>
2020-05-21 01:43:09 +0800

    Massively simplified AnimationTree
    The jump animation has been incorporated well, and also hints at a custom animation playing system that works well with movement mechanics

--- PGBR_Project1/Scripts/animator.gd
+++ PGBR_Project1/Scripts/animator.gd
@@ -7,6 +7,7 @@ export var interpolation := 4.5
 var _strafe_factor: float
 var _interp_speed: float
 var _animation_player: AnimationPlayer
+var _delay := 0.0
 
 
 func _ready():
@@ -15,7 +16,7 @@ func _ready():
 	get_parent().connect("jumped", self, "jump")
 
 
-func ferp(value: float, target: float, t:=_interp_speed) -> float:
+func flerp(value: float, target: float, t:=_interp_speed) -> float:
 	# float interpolation
 	return value + (target - value) * t
 
@@ -28,65 +29,51 @@ func get_parameter(name: String, value_name:="blend_amount"):
 	return get("parameters/" + name + "/" + value_name)
 
 
-func ferp_parameter(name: String, target: float, value_name:="blend_amount") -> void:
-	set_parameter(name, ferp(get_parameter(name, value_name), target), value_name)
+func flerp_parameter(name: String, target: float, value_name:="blend_amount") -> void:
+	set_parameter(name, flerp(get_parameter(name, value_name), target), value_name)
+
+
+func flerp_position(name: String, target: Vector2) -> void:
+	set_parameter(name, get_parameter(name, "blend_position").linear_interpolate(target, _interp_speed), "blend_position")
 
 
 func jump(strength: float):
-	set_parameter("JumpOneShot", true, "active")
+	_delay = 0.4 * strength
+	set("parameters/seek/seek_position", 0)
 
 
 func _process(delta):
 	_interp_speed = interpolation * delta
 	
+	if _delay > 0:
+		_delay -= delta
+		flerp_parameter("custom", 1)
+		return
+	else:
+		flerp_parameter("custom", 0)
+	
 	if get_parent().on_floor:
-		var local_vector := get_parent().global_transform.basis.xform_inv(get_parent().movement_vector).normalized() as Vector3
-		ferp_parameter("falling", 0)
+		flerp_parameter("falling", 0)
 		
-		if get_parent().crouching:
-			ferp_parameter("stand v crouch", 1)
+		if is_zero_approx(get_parent().movement_vector.length_squared()):
+			flerp_parameter("idle", 1)
 			
-			if get_parent().movement_vector.length() > 0:
-				ferp_parameter("crouch idle", 1)
-				ferp_parameter("crouch strafe", local_vector.x)
-				
-				if local_vector.z > 0:
-					ferp_parameter("crouch forward", 0)
-				
-				else:
-					ferp_parameter("crouch forward", 1)
-			
-			else:
-				ferp_parameter("crouch idle", 0)
-		
 		else:
-			ferp_parameter("stand v crouch", 0)
-			
-			if get_parent().movement_vector.length() > 0:
-				ferp_parameter("stand idle", 1)
-			
-				if get_parent().sprinting:
-					ferp_parameter("run v sprint", 1)
-					ferp_parameter("sprint strafe", local_vector.x)
-						
-					if local_vector.z > 0:
-						ferp_parameter("sprint forward", 0)
-					
-					else:
-						ferp_parameter("sprint forward", 1)
-				
-				else:
-					ferp_parameter("run v sprint", 0)
-					ferp_parameter("run strafe", local_vector.x)
-						
-					if local_vector.z > 0:
-						ferp_parameter("run forward", 0)
-					
-					else:
-						ferp_parameter("run forward", 1)
-			
+			flerp_parameter("idle", 0)
+			var local_vector = get_parent().global_transform.basis.xform_inv(get_parent().movement_vector).normalized()
+			local_vector = Vector2(local_vector.x, local_vector.z)
+			print(local_vector)
+			if get_parent().sprinting:
+				flerp_parameter("speed", 1)
+			elif get_parent().crouching:
+				flerp_parameter("speed", -1)
 			else:
-				ferp_parameter("stand idle", 0)
+				flerp_parameter("speed", 0)
+			
+			flerp_position("crouch", local_vector)
+			flerp_position("run", local_vector)
+			flerp_position("sprint", local_vector)
 		
 	else:
-		ferp_parameter("falling", 1)
+		flerp_parameter("idle", 1)
+		flerp_parameter("falling", 1)
