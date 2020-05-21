1347fcb6370fcfda8830f62baa2f57c17147f64b
manglemix <38180553+manglemix@users.noreply.github.com>
2020-05-21 14:13:20 +0800

    Added ignore_origin to mirror_bone
    This allows mirror_bone to only copy the parent rotation onto a bone

--- PGBR_Project1/Scripts/mirror_bone.gd
+++ PGBR_Project1/Scripts/mirror_bone.gd
@@ -5,10 +5,16 @@ extends Node
 
 export var _skeleton_path: NodePath
 export var bone_name: String
+export var ignore_origin := true
 
 onready var _skeleton := get_node(_skeleton_path) as Skeleton
 onready var bone_idx := _skeleton.find_bone(bone_name) as int
 
 
 func _process(_delta):
-	_skeleton.set_bone_pose(bone_idx, get_parent().transform)
+	if ignore_origin:
+		var tmp := get_parent().transform as Transform
+		tmp.origin  = Vector3.ZERO
+		_skeleton.set_bone_pose(bone_idx, tmp)
+	else:
+		_skeleton.set_bone_pose(bone_idx, get_parent().transform)
