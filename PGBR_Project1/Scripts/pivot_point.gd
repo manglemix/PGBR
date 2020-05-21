176f968addccdcfad370f3ae205ac3f727448f69
manglemix <38180553+manglemix@users.noreply.github.com>
2020-05-21 17:28:13 +0800

    Re-added fallback_node to PivotPoint
    I realised there are use cases for it, and it should be left inside for future convenience

--- PGBR_Project1/Scripts/pivot_point.gd
+++ PGBR_Project1/Scripts/pivot_point.gd
@@ -1,19 +1,21 @@
-# Enforces rotation limits, applies excess rotation onto the parent, and can be made to target a global position
+# Enforces rotation limits, applies excess rotation onto the fallback_node, and can be made to target a global position
 class_name PivotPoint
 extends Spatial
 
 
+export var _fallback_node_path := NodePath('..')		# the path to the fallback_node
 export var max_pitch := 70.0
 export var min_pitch := - 70.0
 export var max_yaw := 45.0
 export var min_yaw := -45.0
-export var limit_pitch := true		# if true, excess rotation in this axis will not be applied to the parent
+export var limit_pitch := true		# if true, excess rotation in this axis will not be applied to the fallback_node
 export var limit_yaw := false
 export var turn_speed := 6.0		# used for interpolating to the target orientation
-export var sticky_parent := false	# if true, the parent will always try to turn to its zero point
+export var sticky_parent := false	# if true, the fallback_node will always try to turn to its zero point
 
 var _target_transform: Transform
 
+onready var fallback_node := get_node(_fallback_node_path) as Spatial			# the node which excess rotation from this node will be dumped onto
 onready var _initial_transform := transform		# the initial orientation will become the zero point
 
 
@@ -22,7 +24,7 @@ func _ready():
 
 
 func biaxial_rotate(x: float, y: float) -> void:
-	global_rotate(get_parent().global_transform.basis.y, y)
+	global_rotate(fallback_node.global_transform.basis.y, y)
 	rotate_object_local(Vector3.RIGHT, x)
 
 
@@ -31,7 +33,7 @@ func turn_to_vector(rel_vec: Vector3):
 
 
 func global_turn_to_vector(position: Vector3):
-	_target_transform = global_transform.looking_at(position, get_parent().global_transform.basis.y)
+	_target_transform = global_transform.looking_at(position, fallback_node.global_transform.basis.y)
 	set_process(true)
 
 
@@ -51,17 +53,17 @@ func _process(delta):
 func _physics_process(delta):
 	if sticky_parent:
 		var parent_transform: Transform
-		if get_parent().has_method("get_final_transform"):
-			parent_transform = get_parent().get_final_transform()
+		if fallback_node.has_method("get_final_transform"):
+			parent_transform = fallback_node.get_final_transform()
 		
 		else:
-			parent_transform = get_parent().transform
+			parent_transform = fallback_node.transform
 		
 		var euler_rotation := parent_transform.basis.get_euler()
 		
-		# we do it this way instead of using biaxial_rotate just in case the parent is not a PivotPoint
-		get_parent().global_rotate(get_parent().get_parent().global_transform.basis.y, - euler_rotation.y)
-		get_parent().rotate_object_local(Vector3.RIGHT, - euler_rotation.x)
+		# we do it this way instead of using biaxial_rotate just in case the fallback_node is not a PivotPoint
+		fallback_node.global_rotate(fallback_node.fallback_node.global_transform.basis.y, - euler_rotation.y)
+		fallback_node.rotate_object_local(Vector3.RIGHT, - euler_rotation.x)
 		
 		biaxial_rotate(euler_rotation.x, euler_rotation.y)
 	
@@ -73,24 +75,24 @@ func _physics_process(delta):
 	
 	if euler_rotation.y > max_yaw:
 		if not limit_yaw:
-			get_parent().global_rotate(get_parent().global_transform.basis.y, deg2rad(euler_rotation.y - max_yaw))
+			fallback_node.global_rotate(fallback_node.global_transform.basis.y, deg2rad(euler_rotation.y - max_yaw))
 		
-		global_rotate(get_parent().global_transform.basis.y, deg2rad(max_yaw - euler_rotation.y))
+		global_rotate(fallback_node.global_transform.basis.y, deg2rad(max_yaw - euler_rotation.y))
 		
 	elif euler_rotation.y < min_yaw:
 		if not limit_yaw:
-			get_parent().global_rotate(get_parent().global_transform.basis.y, deg2rad(euler_rotation.y - min_yaw))
+			fallback_node.global_rotate(fallback_node.global_transform.basis.y, deg2rad(euler_rotation.y - min_yaw))
 		
-		global_rotate(get_parent().global_transform.basis.y, deg2rad(min_yaw - euler_rotation.y))
+		global_rotate(fallback_node.global_transform.basis.y, deg2rad(min_yaw - euler_rotation.y))
 
 	if euler_rotation.x > max_pitch:
 		if not limit_pitch:
-			get_parent().global_rotate(global_transform.basis.x, deg2rad(euler_rotation.x - max_pitch))
+			fallback_node.global_rotate(global_transform.basis.x, deg2rad(euler_rotation.x - max_pitch))
 		
 		rotate_object_local(Vector3.RIGHT, deg2rad(max_pitch - euler_rotation.x))
 		
 	elif euler_rotation.x < min_pitch:
 		if not limit_pitch:
-			get_parent().global_rotate(global_transform.basis.x, deg2rad(euler_rotation.x - min_pitch))
+			fallback_node.global_rotate(global_transform.basis.x, deg2rad(euler_rotation.x - min_pitch))
 		
 		rotate_object_local(Vector3.RIGHT, deg2rad(min_pitch - euler_rotation.x))
