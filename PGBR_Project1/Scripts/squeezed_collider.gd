650fbb72f486e4bd19f18a65ecf7ac8d2755798e
manglemix <38180553+manglemix@users.noreply.github.com>
2020-05-21 14:37:15 +0800

    Added small convenience to SqueezedCollider
    Now the SqueezedCollider can move itself into position if a transform parent was not given

--- PGBR_Project1/Scripts/squeezed_collider.gd
+++ PGBR_Project1/Scripts/squeezed_collider.gd
@@ -3,19 +3,24 @@ class_name SqueezedCollider
 extends CollisionShape
 
 
-export var top_node_path: NodePath
-export var transform_parent_path: NodePath
-export var bottom_node_path: NodePath
+export var _top_node_path: NodePath
+export var _transform_parent_path: NodePath		
+export var _bottom_node_path: NodePath
 
-onready var _top_node := get_node(top_node_path) as Spatial
-onready var _bottom_node := get_node(bottom_node_path)
-onready var _transform_parent := get_node(transform_parent_path) as RemoteTransform
+var transform_parent: Spatial
+
+onready var top_node := get_node(_top_node_path) as Spatial
+onready var bottom_node := get_node(_bottom_node_path)
 
 
 func _ready():
 	assert(shape is CapsuleShape)
+	if _transform_parent_path.is_empty():
+		transform_parent = self
+	else:
+		transform_parent = get_node(_transform_parent_path)
 
 
 func _process(delta):
-	shape.height = _top_node.global_transform.origin.y - _bottom_node.global_transform.origin.y - 2 * shape.radius
-	_transform_parent.global_transform.origin.y = _bottom_node.global_transform.origin.y + shape.height / 2 + shape.radius
+	shape.height = top_node.global_transform.origin.y - bottom_node.global_transform.origin.y - 2 * shape.radius
+	transform_parent.global_transform.origin.y = bottom_node.global_transform.origin.y + shape.height / 2 + shape.radius
