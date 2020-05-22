68028bd7da78955a46c0a4b409d898848be009e2
manglemix <38180553+manglemix@users.noreply.github.com>
2020-05-22 17:00:23 +0800

    StateTree only manages one group only
    It was unnecesary to have an AI manage multiple groups

--- PGBR_Project1/UtilityAI/state_tree.gd
+++ PGBR_Project1/UtilityAI/state_tree.gd
@@ -2,22 +2,21 @@ class_name UtilityTree
 extends Node
 
 
+export var group_name: String
+
+
 func _process(_delta):
-	for group in get_groups():
-		if group == "idle_process":
+	for employee in get_tree().get_nodes_in_group(group_name):
+		if employee == self:
 			continue
 		
-		for employee in get_tree().get_nodes_in_group(group):
-			if employee == self:
-				continue
+		var state = employee.get_node_or_null(group_name + "State")
+		
+		if not is_instance_valid(state):
+			# if there was no state assigned to the employee, give it one
+			get_child(0).get_score(employee)	# sometimes some nodes need their score checked first
+			state = get_child(0).get_state(employee)
+			state.name = group_name + "State"
 			
-			var state = employee.get_node_or_null(group + "State")
-			
-			if not is_instance_valid(state):
-				# if there was no state assigned to the employee, give it one
-				get_child(0).get_score(employee)	# sometimes some nodes need their score checked first
-				state = get_child(0).get_state(employee)
-				state.name = group + "State"
-				
-				employee.add_child(state)
-				state.set_process(true)
+			employee.add_child(state)
+			state.set_process(true)
