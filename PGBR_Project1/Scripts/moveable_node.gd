1918379491756029e41e724a22c293cc401a3cef
manglemix <38180553+manglemix@users.noreply.github.com>
2020-05-30 15:44:44 +0800

    Reverted last refactor
    It wasn't actually possible to use to_local on Transforms

--- PGBR_Project1/Scripts/moveable_node.gd
+++ PGBR_Project1/Scripts/moveable_node.gd
@@ -37,9 +37,9 @@ func global_goto(target: Transform):
 		set_process(true)
 	
 	if interpolated:
-		target_transform = get_parent().to_local(target)
+		target_transform = target * get_parent().global_transform.affine_inverse()
 	else:
-		transform = get_parent().to_local(target)
+		transform = target * get_parent().global_transform.affine_inverse()
 
 
 func set_transform_index(index: int):
