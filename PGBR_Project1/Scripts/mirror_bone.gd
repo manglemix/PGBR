e794b9a96db1217e2e4e90d41c68db88880bc276
manglemix <38180553+manglemix@users.noreply.github.com>
2020-05-31 18:16:03 +0800

    Update mirror_bone.gd
    This new script now uses BoneData

--- PGBR_Project1/Scripts/mirror_bone.gd
+++ PGBR_Project1/Scripts/mirror_bone.gd
@@ -1,22 +1,10 @@
 # Mirrors the transform of the parent to the bone of the same name as the node, in the skeleton given
+tool
 class_name MirrorBone
-extends Node
+extends BoneData
 
 
-export var _skeleton_path: NodePath
-export var bone_name: String		# if not given, the name of the node is used instead
 export var ignore_origin := true	# if true, the parent's transform is not applied to the bone
-
-var bone_idx: int
-
-onready var skeleton := get_node(_skeleton_path) as Skeleton
-
-
-func _ready():
-	if bone_name.empty():
-		bone_idx = skeleton.find_bone(name)
-	else:
-		bone_idx = skeleton.find_bone(bone_name)
 
 
 func _process(_delta):
