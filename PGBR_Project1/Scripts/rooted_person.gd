772357d7f735aec1388d69b282cbf552cd2b8664
manglemix <38180553+manglemix@users.noreply.github.com>
2020-06-01 18:33:26 +0800

    Added documentation to RootedPerson

--- PGBR_Project1/Scripts/rooted_person.gd
+++ PGBR_Project1/Scripts/rooted_person.gd
@@ -3,6 +3,7 @@ extends BasePerson
 
 
 func _integrate_velocity(vector: Vector3, delta: float) -> Vector3:
+	# this will check for root motion, and move the Person correspondingly
 	var tmp := global_transform
 	tmp.origin = Vector3.ZERO
 	tmp *= $Animator.get_root_motion_transform()
