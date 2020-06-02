eca0629a7e1324e80459749bbdb8567b73c30cf4
manglemix <38180553+manglemix@users.noreply.github.com>
2020-06-02 18:30:54 +0800

    Removed BoneData

--- PGBR_Project1/Scripts/mirror_bone.gd
+++ PGBR_Project1/Scripts/mirror_bone.gd
@@ -1,15 +1,21 @@
-# Mirrors the transform of the parent to the bone of the same name as the node, in the skeleton given
+# Mirrors the transform of the parent to the bone of the same name as the node, in the _skeleton given
 tool
 class_name MirrorBone
-extends BoneData
+extends Spatial
 
-export var target_path := NodePath("..")
+
+export var _skeleton_path := NodePath("..")
+export var _bone_name: String
+export var use_own_transform := false
 export var active := true setget set_active
-export var compensate_rest := false		# if true, it is assumed that the parent's transform should be the absolute transform, and thus includes the rest pose
-export var ignore_origin := true		# if true, the target's position is not applied to the bone
-export var override_animation := false
+export var compensate_rest := false			# if true, it is assumed that the parent's transform should be the absolute transform, and thus includes the rest pose
+export var ignore_origin := true			# if true, the target's position is not applied to the bone
+export var override_animation := false		# if true, any animation applied to the target bone is overwritten
 
-onready var target := get_node(target_path) as Spatial
+var dont_save := ["_skeleton", "_bone_name"]
+
+onready var _skeleton := get_node(_skeleton_path) as Skeleton
+onready var _bone_idx := _skeleton.find_bone(_bone_name)
 
 
 func set_active(value: bool):
@@ -18,7 +24,7 @@ func set_active(value: bool):
 
 
 func reset():
-	skeleton.set_bone_pose(bone_idx, Transform.IDENTITY)
+	_skeleton.set_bone_pose(_bone_idx, Transform.IDENTITY)
 
 
 func _ready():
@@ -27,18 +33,26 @@ func _ready():
 
 
 func _process(_delta):
-	var tmp := target.transform as Transform
+	var tmp: Transform
+	if use_own_transform:
+		tmp = transform
+	else:
+		tmp = get_parent().transform
 	
 	if compensate_rest:
-		tmp *= skeleton.get_bone_rest(bone_idx).affine_inverse()
+		tmp *= _skeleton.get_bone_rest(_bone_idx).affine_inverse()
 	
 	if ignore_origin:
 		tmp.origin  = Vector3.ZERO
 	
 	if override_animation:
-		tmp = target.get_parent().global_transform * tmp
-		tmp = skeleton.global_transform.affine_inverse() * tmp
-		skeleton.set_bone_global_pose_override(bone_idx, tmp, 1.0, true)
+		if use_own_transform:
+			tmp = get_parent().global_transform * tmp
+		else:
+			tmp = get_parent().get_parent().global_transform * tmp
+		
+		tmp = _skeleton.global_transform.affine_inverse() * tmp
+		_skeleton.set_bone_global_pose_override(_bone_idx, tmp, 1.0, true)
 		
 	else:
-		skeleton.set_bone_pose(bone_idx, tmp)
+		_skeleton.set_bone_pose(_bone_idx, tmp)
