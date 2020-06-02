56e81b771f461e50518d159f7e1516cc3550df67
manglemix <38180553+manglemix@users.noreply.github.com>
2020-06-02 15:49:57 +0800

    RestBone can only be child of Skeleton

--- PGBR_Project1/Scripts/rest_bone.gd
+++ PGBR_Project1/Scripts/rest_bone.gd
@@ -1,8 +1,14 @@
 # Similar to BoneAttachment, but instead sets it's local transform to its bone's rest transform
 tool
 class_name RestBoneAttachment
-extends BoneData
+extends Spatial
+
+
+export var _bone_name: String
+
+onready var _bone_idx := get_parent().find_bone(_bone_name) as int
+onready var _bone_parent := get_parent().get_bone_parent(_bone_idx) as int
 
 
 func _process(_delta):
-	transform = skeleton.get_bone_global_pose(skeleton.get_bone_parent(bone_idx)) * skeleton.get_bone_rest(bone_idx)
+	transform = get_parent().get_bone_global_pose(_bone_parent) * get_parent().get_bone_rest(_bone_idx)
