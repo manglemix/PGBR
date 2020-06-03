07477c8ba120690f0fe7bab306c050e4bcd4beb6
manglemix <38180553+manglemix@users.noreply.github.com>
2020-06-03 19:29:39 +0800

    Added reloading

--- proxima_nova/scripts/ballistic_gun.gd
+++ proxima_nova/scripts/ballistic_gun.gd
@@ -6,10 +6,13 @@ extends Hitscan
 signal total_ammo_changed(total_ammo)
 signal clip_size_changed(clip_size)
 signal clip_ammo_changed(clip_ammo)
+signal reloading
+signal reloaded
 
 export var total_ammo: int setget set_total_ammo
 export var clip_size: int setget set_clip_size
 export var clip_ammo: int setget set_clip_ammo
+export var reload_time: float
 
 
 func set_total_ammo(value: int):
@@ -30,3 +33,13 @@ func set_clip_ammo(value: int):
 	assert(value >= 0)
 	clip_ammo = value
 	emit_signal("ammo_changed", clip_ammo)
+
+
+func reload() -> void:
+	if clip_ammo == clip_size:
+		return
+	
+	emit_signal("reloading")
+	var timer = get_tree().create_timer(reload_time)
+	# let the timer emit the signal "reloaded" for us
+	timer.connect("timeout", self, "emit_signal", ["reloaded"])
