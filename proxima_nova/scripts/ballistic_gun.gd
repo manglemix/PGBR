99c88ba3aff90e8347aa3c946b5ec4d9a2d20b11
manglemix <38180553+manglemix@users.noreply.github.com>
2020-06-03 19:12:55 +0800

    Create ballistic_gun.gd
    Added the ammo management code

---
+++ proxima_nova/scripts/ballistic_gun.gd
@@ -0,0 +1,31 @@
+# For weapons which use bullets and have reload times
+extends Hitscan
+
+
+signal total_ammo_changed(total_ammo)
+signal clip_size_changed(clip_size)
+signal clip_ammo_changed(clip_ammo)
+
+export var total_ammo: int setget set_total_ammo
+export var clip_size: int setget set_clip_size
+export var clip_ammo: int setget set_clip_ammo
+
+
+func set_total_ammo(value: int):
+	assert(value >= 0)
+	total_ammo = value
+	emit_signal("total_ammo_changed", total_ammo)
+	
+	if total_ammo < clip_ammo:
+		set_clip_ammo(total_ammo)
+
+
+func set_clip_size(value: int):
+	clip_size = value
+	emit_signal("clip_size_changed", clip_size)
+
+
+func set_clip_ammo(value: int):
+	assert(value >= 0)
+	clip_ammo = value
+	emit_signal("ammo_changed", clip_ammo)
