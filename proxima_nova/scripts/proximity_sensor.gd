96cc70935fac8f6ec70ba00a1d94d8c070709a8b
manglemix <38180553+manglemix@users.noreply.github.com>
2020-06-04 16:40:41 +0800

    Added documentation to proximity_sensor

--- proxima_nova/scripts/proximity_sensor.gd
+++ proxima_nova/scripts/proximity_sensor.gd
@@ -1,8 +1,8 @@
 extends Area
 
 
-signal detected
-signal body_nearby
+signal detected			# emitted when an object enters the outer collision shape
+signal body_nearby		# emitted when an object enters the inner collision shape
 
 
 enum {DETECTED, NEARBY}
@@ -14,10 +14,12 @@ func _ready():
 
 
 func get_nearby_bodies() -> Array:
+	# returns the overlapping bodies within the inner collision shape
 	return $Close.get_overlapping_bodies()
 
 
 func get_proximities() -> Dictionary:
+	# Returns a dict where the keys are overlapping bodies and the items are enums corresponding to distance
 	var bodies := {}
 		
 	for body in get_overlapping_bodies():
