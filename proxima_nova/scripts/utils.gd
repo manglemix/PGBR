631f8c6bea715b4dc9a05fc88eac955c7b104efe
Vickylance <vignesh6993@live.in>
2020-06-11 00:51:07 +0530

    merge done

--- proxima_nova/scripts/utils.gd
+++ proxima_nova/scripts/utils.gd
@@ -2,3 +2,8 @@ extends Node
 
 func map(x: float, in_min: float, in_max: float, out_min: float, out_max: float):
 	return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min
+
+func reparent(source: Node, target: Node) -> void:
+	source.get_parent().remove_child(source)
+	target.add_child(source)
+	source.set_owner(target)
